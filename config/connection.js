29686681910c585469f169098d1a0cc7093ff516
fatimaromagnoli <fatinhadorei@hotmail.com>
2020-05-01 13:03:31 -0700

    add img

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        port: 3030,
+        port: 3306,
         host: "localhost",
         user: "root",
         password: "password",
